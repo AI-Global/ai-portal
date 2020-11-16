3982cd97ccd5a1f825f9c8b47f682da9a09b541c
Shrivu Shankar <shrivu1122@gmail.com>
2020-11-16 12:15:26 -0600

    email lib

---
+++ api/lib/emails/createAccount.email.js
@@ -0,0 +1,4 @@
+module.exports.subject = ({}) => `AI Global: New Account`;
+module.exports.text = ({}) => `Thanks for creating your account.`;
+module.exports.html = ({}) =>
+  `<strong>Thanks for creating your account.<strong>`;
---
+++ api/lib/emails/resetPassword.email.js
@@ -0,0 +1,3 @@
+module.exports.subject = ({}) => `AI Global: Password Reset`;
+module.exports.text = ({}) => `Your password has been reset.`;
+module.exports.html = ({}) => `<strong>Your password has been reset.<strong>`;
