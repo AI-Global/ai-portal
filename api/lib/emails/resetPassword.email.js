c2845ada40b6cacdcee22985da8b77d75908e910
Martha Czernuszenko <mvczer@gmail.com>
2020-11-17 23:12:41 -0600

    update

--- api/lib/emails/resetPassword.email.js
+++ api/lib/emails/resetPassword.email.js
@@ -1,4 +1,4 @@
-module.exports.subject = ({}) => `AI Global Portal: Password Reset`;
+module.exports.subject = ({}) => `AI Global: Password Reset`;
 module.exports.text = ({ resetURL }) =>
   `You have requested your password on AI Global to be reset. Visit ${resetURL} to reset your password.`;
 module.exports.html = ({ resetURL }) =>
