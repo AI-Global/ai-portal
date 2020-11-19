9cc675b24b4bd28982fa28a066b39ab7709218cf
Martha Czernuszenko <mvczer@gmail.com>
2020-11-18 19:59:24 -0600

    added space

--- api/lib/emails/createAccount.email.js
+++ api/lib/emails/createAccount.email.js
@@ -3,5 +3,5 @@ module.exports.text = ({ name, verifyUrl }) =>
   `Hi ${name}. Welcome to the AI Global portal! Your new account has been created for the AI Global Responsible AI Portal.
     To complete the registration, please verify your email${verifyUrl}`;
 module.exports.html = ({ name, verifyUrl }) =>
-  `<strong>Hi ${name}.Welcome to the AI Global portal! Your new account has been created for the AI Global Responsible AI Portal.
+  `<strong>Hi ${name}. Welcome to the AI Global portal! Your new account has been created for the AI Global Responsible AI Portal.
     To complete the registration, please verify your email ${verifyUrl}<strong>`;
