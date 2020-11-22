3cf5c1b005d807ce8c6e0af565426f7c83ee7e23
Martha Czernuszenko <mvczer@gmail.com>
2020-11-21 22:13:32 -0600

    Added Thanks and AI Global logo

--- api/lib/emails/resetPassword.email.js
+++ api/lib/emails/resetPassword.email.js
@@ -1,5 +1,5 @@
 module.exports.subject = ({}) => `AI Global: Password Reset`;
 module.exports.text = ({ resetURL }) =>
-  `You have requested your password on AI Global to be reset. Visit ${resetURL} to reset your password.`;
+  `You have requested your password on AI Global to be reset. Visit ${resetURL} to reset your password. \n Thanks, AI Global`;
 module.exports.html = ({ resetURL }) =>
-  `<strong>You have requested your password on AI Global to be reset. Visit ${resetURL} to reset your password.<strong>`;
+  `<strong>You have requested your password on AI Global to be reset. Visit ${resetURL} to reset your password.<strong> <br> Thanks, AI Global <br> <img src="https://portal.dev.ai-global.org/logo.png"/>`;
