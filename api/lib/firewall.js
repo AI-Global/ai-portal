3b7676e2877fae51593d6e316f672655c6ddf19d
lucinda <lucinda.onguyen@gmail.com>
2021-02-17 17:41:40 -0600

    user upload resource

--- api/lib/firewall.js
+++ api/lib/firewall.js
@@ -42,6 +42,9 @@ let protect = (method, app, endpoint, endpointHandler, rules, ownerTest) => {
     }
     let user = await req.getUser();
     if (user) {
+      if (user.role == 'member' && firewall(keys, 'user', rules)) {
+        return endpointHandler(req, res);
+      }
       if (user.role == 'mod' && firewall(keys, 'mod', rules)) {
         return endpointHandler(req, res);
       } else if (user.role == 'admin' && firewall(keys, 'admin', rules)) {
