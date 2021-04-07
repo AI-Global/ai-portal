fa3c9f2f0859b7503bd7ed1a2e59550c129576d8
Mayank Shouche <shouchem@utexas.edu>
2021-04-06 22:56:05 -0500

    Discussion forum schema

--- api/models/discussionpost.util.js
+++ api/models/discussionpost.util.js
@@ -3,6 +3,41 @@ const DiscussionPost = mongoose.model('DiscussionPost');
 
 exports.DiscussionPost = DiscussionPost;
 
-exports.create = async (user, text, timestamp, ) => {
+exports.create = async (user, text, timestamp, types = [], paths = []) => {
+  let discussionPost = new DiscussionPost({
+    user: user,
+    text: text,
+    timestamp: timestamp,
+    types: types,
+    paths: paths,
+  });
 
+  await discussionPost.save(); 
+};
+
+exports.get = async postId => {
+  return DiscussionPost.findById(postId);
+};
+
+exports.addReply = async (user, text, timestamp, postId) => {
+  let comment = new Comment({
+    user: user,
+    text: text,
+    timestamp: timestamp,
+    discussionPost: postId,
+  });
+  await comment.save();
+
+  await DiscussionPost.updateOne(
+      { _id: postId },
+      { $push: { comments: comment._id } }
+    );
+};
+
+exports.delete = async postId => {
+  await DiscussionPost.deleteOne({ _id: postId });
 }
+
+exports.upvote = async postId => {
+  await DiscussionPost.updateOne({ _id: postId }, { $inc: { upvotes: 1 } });
+};
