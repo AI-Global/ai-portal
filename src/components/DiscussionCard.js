19913156c1c2d153d20731e70c842170f87ed372
Stephen <stephen8chang@gmail.com>
2021-04-18 15:06:38 -0500

    Finalized UI Design for Discussion Forum Card

--- src/components/DiscussionCard.js
+++ src/components/DiscussionCard.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import CommentsList from './CommentsList';
-import { Card, Avatar, Row, Col, Statistic } from '../ant';
+import { Card, Row, Col, Statistic, Tag } from '../ant';
 import { EditOutlined, EllipsisOutlined, SettingOutlined, CommentOutlined } from '@ant-design/icons';
 import Upvote from './Upvote';
 
@@ -11,39 +11,61 @@ export default function DiscussionCard({ }) {
     //     <EditOutlined key="edit" />,
     //     <EllipsisOutlined key="ellipsis" />,
     // ]}
+    let tags = ["Media", "Retail", "Other"]
     return (
         <div className="resource-box">
             <Card
                 hoverable
                 style={{ width: 965 }}
+                title={
+                    <>
+                        <Row gutter={[24, 4]}>
+                            <Col align="center" style={{ padding: '14px 0 0 14px' }}>
+                                <b>Ethical Implications of AI</b>
+                            </Col>
+                            <Col align="center" style={{ padding: '14px 0 0 14px' }}>
+                                {tags.map(t => (
+                                    <Tag
+                                        style={{
+                                            color: 'white',
+                                            fontWeight: 'bold',
+                                            align: "left"
+                                        }}
+                                        color={'#00CDFF'}
+                                    >
+                                        {t.toUpperCase()}
+                                    </Tag>
+                                ))}
+                            </Col>
+                        </Row>
+                    </>
+                }
             >
                 <Card.Meta
-                    title="Ethical Implications of AI"
-                    description={
-                        <div>
-                            <p>Written by User 1337, Tags: Media, Retail, Other</p>
-                            <p>Optimizing logistics, detecting fraud, composing art, conducting research, providing translations: intelligent machine systems are transforming our lives for the better. As these systems become more capable, our world becomes more efficient and consequently richer.</p>
-                        </div>
-                    }
-                />
+                    description={<b>Written by User 1337</b>}
+                ></Card.Meta>
+                <Card.Meta description={<p>Optimizing logistics, detecting fraud, composing art, conducting research, providing translations: intelligent machine systems are transforming our lives for the better. As these systems become more capable, our world becomes more efficient and consequently richer.</p>}></Card.Meta>
                 <div>
                     <Row gutter={[24, 4]}>
                         <Col align="center" style={{ padding: '14px 0 0 14px' }}>
-                            <Upvote size={20} /*userUpvoted={ } onClick={ }*/ />
-                            <Statistic value={5} valueStyle={{ fontSize: 20 }} />
+                            <Upvote size={15} /*userUpvoted={ } onClick={ }*/ />
+                        </Col>
+                        <Col align="center" style={{ padding: '14px 0 0 3px' }}>
+                            <Statistic value={5} valueStyle={{ fontSize: 15 }} />
                         </Col>
                         <Col align="center" style={{ padding: '14px 0 0 14px' }}>
-                            <CommentOutlined style={{ fontSize: 20 }}></CommentOutlined>
-                            <Statistic value={5} valueStyle={{ fontSize: 20 }} />
+                            <CommentOutlined style={{ fontSize: 15 }}></CommentOutlined>
+                        </Col>
+                        <Col align="center" style={{ padding: '14px 0 0 3px' }}>
+                            <Statistic value={5} valueStyle={{ fontSize: 15 }} />
                         </Col>
                         <Col align="center" style={{ padding: '14px 0 0 14px' }}>
-                            <br></br>
-                            <p>Last updated 10s ago</p>
+                            <b>Last updated 10s ago</b>
                         </Col>
                     </Row>
                 </div>
             </Card>
-        </div>
+        </div >
     );
 }
 
